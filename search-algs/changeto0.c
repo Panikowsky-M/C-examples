e1bd0aadeb1af0fd41c56798991dc9eec64e218a
Panikowsky-M <panikowsky.m@gmail.com>
2018-11-14 22:51:53 +0300

    Добавлены два новых исходных текста.

---
+++ search-algs/changeto0.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int transPosSrch(int *x_input, int si, int desi)
+{
+   int tmp;
+    for(int i = 0; i < si; i++)
+	{
+          if(x_input[i] == desi)
+	     tmp = x_input[i];
+	     x_input[i] = x_input[0];
+	     x_input[0] = tmp;
+	     return i;
+	 }
+	return -1;
+}
+
+int main()
+{
+    int tArr[4];
+    int DigForSing, inp_el;
+    DigForSing = scanf("%d", &inp_el);
+    int s;
+    for(s = 0; s < 4; s++)
+	{
+          printf("Enter an element: \n");
+	  scanf(" %d", &tArr[s] );
+         }
+	  
+    for(int SitrP = 0; SitrP < inp_el*2 ; SitrP++)
+	{
+    	  int d; d =  transPosSrch(tArr,4,inp_el);
+ 	  if(d == -1) printf("Element not found !");
+	   else printf("Founded an element idxed w/ %d which is %d\n", d,inp_el);
+	 }
+return 0;
+}
