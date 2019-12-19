e3c04bcc4f6a6eda7a3a5236e9112d43a51c582c
Panikowsky-M <panikowsky.m@gmail.com>
2019-12-19 13:08:33 +0300

    19-12-19 13:07 МСК решена задача №1 из практической №3

--- practice-pp/pr3t1.c
+++ practice-pp/pr3t1.c
@@ -10,8 +10,18 @@ int getLn(char str_inp[])
      return strL;
 }
 
+
 int main()
 {
-    char str0[] = "This string contains many a and b letters: aaaababbabbabababbabaaaaabbabababaa";
-    printf("When getLn() was called, I got %d\n", getLn(str0));
+    char str0[] = "This string contains many a and b letters:bbbbbbbbbbababababababbbbbbbbbb" ;
+    printf("When getLn() was called, I got %d!\n", getLn(str0));
+   char x0 = 'a'; char x1 = 'b';
+   int let0 = 0, let1 = 0;
+   for(int k = 0; k < getLn(str0); k++)
+     {
+       if(str0[k] == x0)let0++;
+       if(str0[k] == x1)let1++;
+       }if(let0 > let1)printf("a letters are more than b.\n"); 
+        else printf("b letters are more than a.\n");
+   return 0;
 }
