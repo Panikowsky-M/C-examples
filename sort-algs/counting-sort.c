ae4af592331a886ba928f238b8b4c41c0670f05c
Владислав <panikowsky.m@gmail.com>
2020-02-24 11:10:18 +0300

    Добавлена реализция сортировки подсчетом

--- sort-algs/counting-sort.c
+++ sort-algs/counting-sort.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <iso646.h>
 
-void print_Ar()
-{
+//void print_Ar()
+//{
    
 
 
@@ -14,10 +15,12 @@ int main()
     while (true)
    	{
 	  scanf("%d",&x);
-    	  if (x == 10) break;
+	  if (x == 10) break;//Число терминатор = 10
           if(x < 0 or x > 9)continue;
 	  Cs[x] +=1;
-
+}for(x = 0; x < 10; ++x)
+	for(int i = 0; i < Cs[x]; ++i)
+	printf("%3d\n",x);
 
 return 0;
 }
